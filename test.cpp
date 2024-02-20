23ddb10a4cd7ff2cc40d3089ac52d7d701ad962f
szabotudor <szabo.tudor@gmail.com>
2024-02-20 21:44:47 +0100

    Added more complex tests

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #if defined(DEBUG) || defined(_DEBUG)
 #define ALLOW_THROW
 #endif
@@ -18,24 +19,56 @@ std::string load_file(const std::string& file) {
 }
 
 
+mgm::Result<std::string, mgm::System::CompilationError> shader_convert(mgm::System& system, mgm::System::Source& src, const std::unordered_map<std::string, std::vector<std::string>>& found_words) {
+    std::string res = "#version 450 core\n";
+    for (const auto& word : found_words.at("var")) {
+        const auto parsed_word = system.parse(word);
+        if (parsed_word.is_error()) {
+            for (const auto& err : parsed_word.error())
+                std::cerr << "Error at " << err.pos.line << ':' << err.pos.column << "\n\t" << err.message << std::endl;
+            return mgm::System::CompilationError{parsed_word.error()[0]};
+        }
+        res += parsed_word.result() + '\n';
+    }
+    return res;
+}
+
+
 int main() {
     const auto test = load_file("test.mmd");
 
     mgm::System mp{};
 
     mp.rules.emplace_back(
-        "   test",
-        "   (",
-        " *$v",
-        " * ,",
-        "   )",
-        "  +\"$($v/$($v), )\""
+        &shader_convert,
+        "^  vertex",
+        "^  fragment",
+        "   {",
+        "   vars:",
+        " *$var",
+        " * ;",
+        "   code:",
+        " *$code",
+        " * ;",
+        "   }"
+    );
+    mp.rules.emplace_back(
+        "   var",
+        "  $type",
+        "  $name",
+        "  +\"uniform $type $name;\""
+    );
+    mp.rules.emplace_back(
+        "   buffer",
+        "  $type",
+        "  $name",
+        "  +\"layout(std140, location = $EXPAND_COUNT) buffer $name { $type $name[]; };\""
     );
 
     const auto bytecode = mp.parse(test);
     if (bytecode.is_error())
         for (const auto& err : bytecode.error())
-            std::cerr << "Error at " << err.pos.line << ':' << err.pos.column << '\n' << err.message << std::endl;
+            std::cerr << "Error at " << err.pos.line << ':' << err.pos.column << "\n\t" << err.message << std::endl;
     else {
         std::cout << bytecode.result() << std::endl;
     }
